1f557c10888dba769267b671686a5c85ce83287a
KelvynMarck <20211278@itla.edu.do>
2022-08-19 19:46:06 -0400

    Splash terminado

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -47,7 +47,7 @@
             this.ProgressBar1.InnerColor = System.Drawing.Color.FromArgb(((int)(((byte)(42)))), ((int)(((byte)(40)))), ((int)(((byte)(60)))));
             this.ProgressBar1.InnerMargin = 2;
             this.ProgressBar1.InnerWidth = -1;
-            this.ProgressBar1.Location = new System.Drawing.Point(49, 112);
+            this.ProgressBar1.Location = new System.Drawing.Point(51, 112);
             this.ProgressBar1.MarqueeAnimationSpeed = 2000;
             this.ProgressBar1.Name = "ProgressBar1";
             this.ProgressBar1.OuterColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(28)))), ((int)(((byte)(43)))));
@@ -74,7 +74,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(213)))), ((int)(((byte)(219)))));
-            this.label1.Location = new System.Drawing.Point(16, 47);
+            this.label1.Location = new System.Drawing.Point(18, 47);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(219, 37);
             this.label1.TabIndex = 1;
@@ -85,7 +85,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(213)))), ((int)(((byte)(219)))));
-            this.label2.Location = new System.Drawing.Point(71, 300);
+            this.label2.Location = new System.Drawing.Point(73, 300);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(113, 23);
             this.label2.TabIndex = 1;
@@ -101,6 +101,11 @@
             this.label3.Size = new System.Drawing.Size(197, 17);
             this.label3.TabIndex = 1;
             this.label3.Text = "CopyRight © KelvynMarck";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
--- Form1.cs
+++ Form1.cs
@@ -10,7 +10,9 @@ namespace Splash
     {
 
         //Creando el metodo
-        [DllImport("Gdi32.d11", EntryPoint = "CreateRoundRectRgn")]
+       
+       [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
 
         private static extern IntPtr CreateRoundReactRgn
 
@@ -27,11 +29,26 @@ namespace Splash
         {
             InitializeComponent();
             Region = System.Drawing.Region.FromHrgn(CreateRoundReactRgn(0, 0, Width, Height, 25, 25));
+            ProgressBar1.Value = 0;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ProgressBar1.Value += 1;
+            ProgressBar1.Text = ProgressBar1.Value.ToString();
+
+            if (ProgressBar1.Value == 100)
+            {
+                timer1.Enabled = false;
+                Marck marck_form = new Marck();
+                marck_form.Show();
+                this.Hide();
+            }
         }
     }
 }
